4031aae4f27b7ad07d11da14fc6bfdc42b55b844
Wai Lwin Phyo <mr.wailwinphyo.mu.phys@gmail.com>
2022-01-14 17:38:07 +0630

    editing todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../reset.css";
 import "../App.css";
 
@@ -8,21 +8,24 @@ function App() {
       id: 1,
       title: "Finish React Series",
       isComplete: false,
+      isEditting: false,
     },
     {
       id: 2,
       title: "Go Grocery",
-      isComplete: true,
+      isComplete: false,
+      isEditting: false,
     },
     {
       id: 3,
       title: "Take over world",
       isComplete: false,
+      isEditting: false,
     },
   ]);
-
   let [inputValue, setInputValue] = useState("");
   let [idForTodo, setIdForTodo] = useState(4);
+  let inputTitle = useRef();
 
   let addTodo = (event) => {
     event.preventDefault();
@@ -47,6 +50,54 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  let completeTodo = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
+  let markAsEditting = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isEditting = !todo.isEditting;
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
+  let updateTodo = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        if (inputTitle.current.value.length === 0) {
+          todo.isEditting = !todo.isEditting;
+          return todo;
+        }
+        todo.title = inputTitle.current.value;
+        todo.isEditting = !todo.isEditting;
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
+  const cancelEdit = (id) => {
+    let updateTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isEditting = !todo.isEditting;
+      }
+      return todo;
+    });
+    setTodos(updateTodo);
+  };
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
@@ -65,9 +116,39 @@ function App() {
           {todos.map((todo, index) => (
             <li className="todo-item-container" key={todo.id}>
               <div className="todo-item">
-                <input type="checkbox" />
-                <span className="todo-item-label">{todo.title}</span>
-                {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+                <input
+                  type="checkbox"
+                  onChange={() => completeTodo(todo.id)}
+                  chacked={todo.isComplete && true}
+                />
+                {!todo.isEditting ? (
+                  <span
+                    onDoubleClick={() => markAsEditting(todo.id)}
+                    className={`todo-item-label ${
+                      todo.isComplete ? "line-through" : ""
+                    }`}
+                  >
+                    {todo.title}
+                  </span>
+                ) : (
+                  <input
+                    onBlur={() => updateTodo(todo.id)}
+                    onKeyDown={(event) => {
+                      console.log(event.key);
+                      if (event.key === "Enter") {
+                        updateTodo(todo.id);
+                      } else if (event.key === "Escape") {
+                        cancelEdit(todo.id);
+                      }
+                    }}
+                    type="text"
+                    className="todo-item-input"
+                    defaultValue={todo.title}
+                    ref={inputTitle}
+                    autoFocus
+                  />
+                  //! In input, defaultValue is only temporary value and you can change later. If you use 'value' , this will be always value.
+                )}
               </div>
               <button className="x-button" onClick={() => deleteTodo(todo.id)}>
                 <svg
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AppClass />
+    <App />
+    {/* <AppClass /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
